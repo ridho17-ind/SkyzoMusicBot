a83b4cf14b82f4ea1291e723355297bdf581b7b9
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2022-01-03 06:22:03 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -4,13 +4,12 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InputMediaPhoto,
     Message,
-from Music.config import GROUP
 )
 
 def play_markup(videoid, user_id):
     buttons= [
             [
-                InlineKeyboardButton(text="Support Chat", url=f"https://t.me/{GROUP}"),
+                InlineKeyboardButton(text="Support Chat", url=f"https://t.me/flicksrobotsupport"),
             ],
             [      
                 InlineKeyboardButton(text="Menu", callback_data=f'other {videoid}|{user_id}'),
