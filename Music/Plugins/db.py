823ed1d34f2324fd9acb30c6840a64b50275816d
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2022-01-02 19:26:55 +0700

    Create db.py

---
+++ Music/Plugins/db.py
@@ -0,0 +1,165 @@
+import asyncio
+
+from pyrogram import filters
+
+from Music import app, SUDOERS
+
+from Music.MusicUtilities.database.chats import get_served_chats
+
+@app.on_message(filters.command("broadcast_pin") & filters.user(SUDOERS))
+async def broadcast_message_pin_silent(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await app.forward_messages(i, y, x)
+                try:
+                    await m.pin(disable_notification=True)
+                    pin += 1
+                except Exception:
+                    pass
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(
+            f"**Broadcasted Message In {sent}  Chats with {pin} Pins.**"
+        )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    pin = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await app.send_message(i, text=text)
+            try:
+                await m.pin(disable_notification=True)
+                pin += 1
+            except Exception:
+                pass
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(
+        f"**Broadcasted Message In {sent} Chats and {pin} Pins.**"
+    )
+
+
+@app.on_message(filters.command("broadcast_pin_loud") & filters.user(SUDOERS))
+async def broadcast_message_pin_loud(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await app.forward_messages(i, y, x)
+                try:
+                    await m.pin(disable_notification=False)
+                    pin += 1
+                except Exception:
+                    pass
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(
+            f"**Broadcasted Message In {sent}  Chats with {pin} Pins.**"
+        )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    pin = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await app.send_message(i, text=text)
+            try:
+                await m.pin(disable_notification=False)
+                pin += 1
+            except Exception:
+                pass
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(
+        f"**Broadcasted Message In {sent} Chats and {pin} Pins.**"
+    )
+
+
+@app.on_message(filters.command("broadcast") & filters.user(SUDOERS))
+async def broadcast(_, message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await app.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(f"**Broadcasted Message In {sent} Chats.**")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**Usage**:\n/broadcast [MESSAGE] or [Reply to a Message]"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await app.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(f"**Broadcasted Message In {sent} Chats.**")
+
