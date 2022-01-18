dd724bdd0d4ce9623cbb63b3646782ba7ae8a5f6
SkyXProject <93299453+SkyXProject@users.noreply.github.com>
2022-01-19 03:24:03 +0700

    Update maintenance.py

--- Music/Plugins/maintenance.py
+++ Music/Plugins/maintenance.py
@@ -5,41 +5,21 @@ from Music.MusicUtilities.database.onoff import (is_on_off, add_on, add_off)
 from Music.MusicUtilities.helpers.filters import command
 
 
-@Client.on_message(command("Musicp") & filters.user(SUDOERS))
+@Client.on_message(command("player") & filters.user(SUDOERS))
 async def smex(_, message):
-    usage = "**Usage:**\n/Musicp [enable|disable]"
+    usage = "**Penggunaan:**\n/player [on|off]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
+    if state == "on":
         user_id = 1
         await add_on(user_id)
-        await message.reply_text("Music Enabled for Maintenance")
-    elif state == "disable":
+        await message.reply_text("✅ Musik Diaktifkan untuk Pemeliharaan")
+    elif state == "off":
         user_id = 1
         await add_off(user_id)
-        await message.reply_text("Maintenance Mode Disabled")
-    else:
-        await message.reply_text(usage)
-
-        
-@Client.on_message(command("stp") & filters.user(SUDOERS))
-async def sls_skfs(_, message):
-    usage = "**Usage:**\n/st [enable|disable]"
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    chat_id = message.chat.id
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
-    if state == "enable":
-        user_id = 2
-        await add_on(user_id)
-        await message.reply_text("Speedtest Enabled")
-    elif state == "disable":
-        user_id = 2
-        await add_off(user_id)
-        await message.reply_text("Speedtest Disabled")
+        await message.reply_text("❌ Mode Pemeliharaan Dinonaktifkan")
     else:
         await message.reply_text(usage)
