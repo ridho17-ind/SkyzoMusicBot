c7bbb05878924093e88882b621d35fc991076a08
SkyXProject <93299453+SkyXProject@users.noreply.github.com>
2022-01-19 02:58:45 +0700

    Update ping.py

--- Music/Plugins/ping.py
+++ Music/Plugins/ping.py
@@ -1,33 +1,33 @@
-from pyrogram import filters, Client
-from pyrogram.types import Message
-from Music import app, SUDOERS, Music_START_TIME
-import os
-import psutil
 import time
 from datetime import datetime
+
+import psutil
+from Music import Music_START_TIME, app
 from Music.MusicUtilities.helpers.time import get_readable_time
+from pyrogram import filters
+
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - Music_START_TIME)
     cpu = psutil.cpu_percent(interval=0.5)
     mem = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-    stats = f'''
-Uptime: {get_readable_time((bot_uptime))}
+    stats = f"""
+Waktu aktif: {get_readable_time((bot_uptime))}
 CPU: {cpu}%
 RAM: {mem}%
-Disk: {disk}%'''
+Disk: {disk}%
+"""
     return stats
 
 
-@app.on_message(filters.command(["pingmusic", "ping@Tg_Vc_00_Bot"]))
+@app.on_message(filters.command("ping"))
 async def ping(_, message):
     uptime = await bot_sys_stats()
     start = datetime.now()
-    response = await message.reply_photo(
-        photo="cache/Query.png",
-        caption=">> Pong!"
-    )
+    response = await message.reply_text("ping...")
     end = datetime.now()
     resp = (end - start).microseconds / 1000
-    await response.edit_text(f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>Music System Stats:</u></b>{uptime}")
+    await response.edit(
+        f"**Pong !!**\n`💫{resp} ms`\n\n<b><u>Statistik Sistem Musik:</u></b>{uptime}"
+    )
