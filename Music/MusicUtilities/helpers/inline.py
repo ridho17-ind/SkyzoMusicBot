64401c03b999d5da0559e93294cffd72258c57fc
SkyXProject <93299453+SkyXProject@users.noreply.github.com>
2022-01-19 01:38:05 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -90,13 +90,13 @@ def audio_markup(videoid, user_id):
 def search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query):
     buttons= [
             [
-                InlineKeyboardButton(text="1️⃣", callback_data=f'Music2 {ID1}|{duration1}|{user_id}'),
-                InlineKeyboardButton(text="2️⃣", callback_data=f'Music2 {ID2}|{duration2}|{user_id}'),
-                InlineKeyboardButton(text="3️⃣", callback_data=f'Music2 {ID3}|{duration3}|{user_id}')
+                InlineKeyboardButton(text="1", callback_data=f'Music2 {ID1}|{duration1}|{user_id}'),
+                InlineKeyboardButton(text="2", callback_data=f'Music2 {ID2}|{duration2}|{user_id}'),
+                InlineKeyboardButton(text="3", callback_data=f'Music2 {ID3}|{duration3}|{user_id}')
             ],
             [ 
-                InlineKeyboardButton(text="4️⃣", callback_data=f'Music2 {ID4}|{duration4}|{user_id}'),
-                InlineKeyboardButton(text="5️⃣", callback_data=f'Music2 {ID5}|{duration5}|{user_id}')
+                InlineKeyboardButton(text="4", callback_data=f'Music2 {ID4}|{duration4}|{user_id}'),
+                InlineKeyboardButton(text="5", callback_data=f'Music2 {ID5}|{duration5}|{user_id}')
             ],
             [ 
                 
@@ -110,13 +110,13 @@ def search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, dura
 def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, duration9, duration10 ,user_id, query):
     buttons= [
             [
-                InlineKeyboardButton(text="6️⃣", callback_data=f'Music2 {ID6}|{duration6}|{user_id}'),
-                InlineKeyboardButton(text="7️⃣", callback_data=f'Music2 {ID7}|{duration7}|{user_id}'),
-                InlineKeyboardButton(text="8️⃣", callback_data=f'Music2 {ID8}|{duration8}|{user_id}')
+                InlineKeyboardButton(text="6", callback_data=f'Music2 {ID6}|{duration6}|{user_id}'),
+                InlineKeyboardButton(text="7", callback_data=f'Music2 {ID7}|{duration7}|{user_id}'),
+                InlineKeyboardButton(text="8", callback_data=f'Music2 {ID8}|{duration8}|{user_id}')
             ],
             [ 
-                InlineKeyboardButton(text="9️⃣", callback_data=f'Music2 {ID9}|{duration9}|{user_id}'),
-                InlineKeyboardButton(text="🔟", callback_data=f'Music2 {ID10}|{duration10}|{user_id}')
+                InlineKeyboardButton(text="9", callback_data=f'Music2 {ID9}|{duration9}|{user_id}'),
+                InlineKeyboardButton(text="10", callback_data=f'Music2 {ID10}|{duration10}|{user_id}')
             ],
             [ 
                 
@@ -143,7 +143,7 @@ start_keyboard = InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "📜 Commands", url="https://telegra.ph/Skyzo-11-10"
+                        "📜 Commands", url="https://telegra.ph/SKY-11-10"
                     )
                 ],
                 [
