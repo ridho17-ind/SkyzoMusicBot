4bd60a32cfbad4afc50d0151dfb80fdf6f733d56
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2022-01-23 09:46:16 +0700

    Update logger.py
    ini doang

--- Music/MusicUtilities/helpers/logger.py
+++ Music/MusicUtilities/helpers/logger.py
@@ -15,14 +15,15 @@ async def LOG_CHAT(message, what):
     user_name = message.from_user.first_name
     mention = "["+user_name+"](tg://user?id="+str(user_id)+")" 
     logger_text = f"""
-__**New {what}**__
+**🚀 New {what}**
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
-**Query:** {message.text}"""
+**• Chat:** {message.chat.title} 
+**• Chat ID:** [`{message.chat.id}`]
+**• User:** {mention}
+**• Username:** @{message.from_user.username}
+**• User ID:** `{message.from_user.id}`
+**• Chat Link:** {chatusername}
+**• Query:** {message.text}"""
     await app.send_message(LOG_GROUP_ID, 
                text = f"{logger_text}", 
                reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="ᴠɪᴇᴡs​​", url=f"https://t.me/{message.from_user.username}")]]),
